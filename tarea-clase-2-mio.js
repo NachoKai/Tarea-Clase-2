2ac85cf0173c458bc9b81dcd0ab770beb9976713
Nacho Caiafa <47578311+NachoKai@users.noreply.github.com>
2019-10-30 18:53:45 -0300

    Update tarea-clase-2-mio.js

--- tarea-clase-2-mio.js
+++ tarea-clase-2-mio.js
@@ -1,62 +1,67 @@
-function sumar(primerNumero, segundoNumero) {
-    return primerNumero + segundoNumero
+function calcularSalarioAnual(salarioMensual) {
+    const cantidadMesesEnUnAnio = 12;
+    return salarioMensual * cantidadMesesEnUnAnio;
 }
 
-function restar(primerNumero, segundoNumero) {
-    return primerNumero - segundoNumero
+function calcularSalarioMensual(salarioAnual) {
+    const cantidadMesesEnUnAnio = 12;
+    return salarioAnual / cantidadMesesEnUnAnio;
 }
 
-function multiplicar(primerNumero, segundoNumero) {
-    return primerNumero * segundoNumero
+function calcularSalarioSemanal(salarioAnual) {
+    const cantidadSemanasEnUnAnio = 52;
+    return salarioAnual / cantidadSemanasEnUnAnio;
 }
 
-function dividir(primerNumero, segundoNumero) {
-    return primerNumero / segundoNumero
+function calcularSalarioDiario(salarioAnual) {
+    const cantidadDiasEnUnAnio = 365;
+    return salarioAnual / cantidadDiasEnUnAnio;
 }
 
-function mostrarResultado() {
-    let $primerNumero = document.querySelector('#primer-numero')
-    let primerNumero = Number($primerNumero.value)
-    let $segundoNumero = document.querySelector('#segundo-numero')
-    let segundoNumero = Number($segundoNumero.value)
-    let nodoResultado = document.querySelector('#resultado')
-    let suma = document.querySelector('#suma')
-    let resta = document.querySelector('#resta')
-    let multiplicacion = document.querySelector('#multiplicacion')
-    let division = document.querySelector('#division')
-    let calculoSuma = sumar(primerNumero, segundoNumero)
-    let calculoResta = restar(primerNumero, segundoNumero)
-    let calculoMultiplicacion = multiplicar(primerNumero, segundoNumero)
-    let calculoDivision = dividir(primerNumero, segundoNumero)
+function mostrarSalarios() {
+    let $salarioMensual = document.querySelector(`#salarioMensual`)
+    let salarioMensual = Number($salarioMensual.value)
 
-    if (suma.checked) {
-        nodoResultado.className = ('valido')
-        nodoResultado.innerText = `${primerNumero} + ${segundoNumero} = ${calculoSuma}`
-    } else if (resta.checked) {
-        nodoResultado.className = ('valido')
-        nodoResultado.innerText = `${primerNumero} - ${segundoNumero} = ${calculoResta}`
-    } else if (multiplicacion.checked) {
-        nodoResultado.className = ('valido')
-        nodoResultado.innerText = `${primerNumero} * ${segundoNumero} = ${calculoMultiplicacion}`
-    } else if (division.checked) {
-        nodoResultado.className = ('valido')
-        nodoResultado.innerText = `${primerNumero} / ${segundoNumero} = ${calculoDivision}`
+    let $salarioAnual = document.querySelector(`#salarioAnual`)
+    let salarioAnual = Number($salarioAnual.value)
+
+    let nodoResultadoSalarioAnual = document.querySelector(`#resultadoSalario`)
+    let resultadoSalarioAnual = calcularSalarioAnual(salarioMensual)
+
+    let nodoResultadoSalarioMensual = document.querySelector(`#resultadoSalario`)
+    let resultadoSalarioMensual = calcularSalarioMensual(salarioAnual)
+
+    if (resultadoSalarioAnual > 0) {
+        nodoResultadoSalarioAnual.className = (`valido`)
+        nodoResultadoSalarioAnual.innerText = `Tu salario Anual es de $${calcularSalarioAnual(salarioMensual).toFixed(2)} Tu salario Semanal es de $${calcularSalarioSemanal(salarioAnual).toFixed(2)} Tu salario Diario es de $${calcularSalarioDiario(salarioAnual).toFixed(2)}`
     } else {
-        nodoResultado.className = ('error')
-        nodoResultado.innerText = 'Completa los campos correctamente'
+        nodoResultadoSalarioAnual.className = (`error`)
+        nodoResultadoSalarioAnual.innerText = `Completa los campos correctamente`
+    }
+
+    if (resultadoSalarioMensual > 0) {
+        nodoResultadoSalarioMensual.className = (`valido`)
+        nodoResultadoSalarioMensual.innerText = `Tu salario Mensual es de $${calcularSalarioMensual(salarioAnual).toFixed(2)} Tu salario Semanal es de $${calcularSalarioSemanal(salarioAnual).toFixed(2)} Tu salario Diario es de $${calcularSalarioDiario(salarioAnual).toFixed(2)}`
+    } else {
+        nodoResultadoSalarioMensual.className = (`error`)
+        nodoResultadoSalarioMensual.innerText = `Completa los campos correctamente`
     }
 }
 
-function reiniciarResultado() {
-    let nodoResultado = document.querySelector('#resultado')
-    nodoResultado.className = ('')
-    nodoResultado.innerText = ''
+function reiniciarSalario() {
+    let nodoResultadoSalarioAnual = document.querySelector(`#resultadoSalario`)
+    nodoResultadoSalarioAnual.className = (``)
+    nodoResultadoSalarioAnual.innerText = ``
+
+    let nodoResultadoSalarioMensual = document.querySelector(`#resultadoSalario`)
+    nodoResultadoSalarioMensual.className = (``)
+    nodoResultadoSalarioMensual.innerText = ``
 }
 
-document.querySelector('#calcular').onclick = function () {
-    mostrarResultado()
+document.querySelector(`#calcularSalario`).onclick = function () {
+    mostrarSalarios()
 }
 
-document.querySelector('#reset').onclick = function () {
-    reiniciarResultado()
+document.querySelector(`#resetSalario`).onclick = function () {
+    reiniciarSalario()
 }
