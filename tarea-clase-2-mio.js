c45efccd592b21e3ffb74d63ef71f232c5248b7b
Nacho Caiafa <47578311+NachoKai@users.noreply.github.com>
2019-10-30 17:44:37 -0300

    Add files via upload

---
+++ tarea-clase-2-mio.js
@@ -0,0 +1,70 @@
+function sumar(primerNumero, segundoNumero) {
+    return primerNumero + segundoNumero;
+}
+
+function restar(primerNumero, segundoNumero) {
+    return primerNumero - segundoNumero;
+}
+
+function multiplicar(primerNumero, segundoNumero) {
+    return primerNumero * segundoNumero;
+}
+
+function dividir(primerNumero, segundoNumero) {
+    return primerNumero / segundoNumero;
+}
+
+function reiniciarResultado() {
+    let nodoResultado = document.querySelector(`#resultado`)
+    nodoResultado.className = (``)
+    nodoResultado.innerText = ``
+}
+
+let suma = document.querySelector(`#suma`)
+let resta = document.querySelector(`#resta`)
+let multiplicacion = document.querySelector(`#multiplicacion`)
+let division = document.querySelector(`#division`)
+let primerNumero = document.querySelector(`#primerNumero`).value
+let segundoNumero = document.querySelector(`#segundoNumero`).value
+let resultado = document.querySelector(`#resultado`)
+
+document.querySelector(`#calcular`).onclick = function () {
+    mostrarResultado()
+}
+
+document.querySelector(`#reset`).onclick = function () {
+    reiniciarResultado()
+}
+
+function mostrarResultado() {
+    let $primerNumero = document.querySelector(`#primerNumero`)
+    let primerNumero = Number($primerNumero.value)
+    let $segundoNumero = document.querySelector(`#segundoNumero`)
+    let segundoNumero = Number($segundoNumero.value)
+    let nodoResultado = document.querySelector(`#resultado`)
+    let suma = document.querySelector(`#suma`)
+    let resta = document.querySelector(`#resta`)
+    let multiplicacion = document.querySelector(`#multiplicacion`)
+    let division = document.querySelector(`#division`)
+    let calculoSuma = sumar(primerNumero, segundoNumero)
+    let calculoResta = restar(primerNumero, segundoNumero)
+    let calculoMultiplicacion = multiplicar(primerNumero, segundoNumero)
+    let calculoDivision = dividir(primerNumero, segundoNumero)
+
+    if (suma.checked = true) {
+        nodoResultado.className = (`valido`)
+        nodoResultado.innerText = `El resultado de ${primerNumero} + ${segundoNumero} es igual a ${calculoSuma}`
+    } else if (resta.checked = true) {
+        nodoResultado.className = (`valido`)
+        nodoResultado.innerText = `El resultado de ${primerNumero} - ${segundoNumero} es igual a ${calculoResta}`
+    } else if (multiplicacion.checked = true) {
+        nodoResultado.className = (`valido`)
+        nodoResultado.innerText = `El resultado de ${primerNumero} * ${segundoNumero} es igual a ${calculoMultiplicacion}`
+    } else if (division.checked = true) {
+        nodoResultado.className = (`valido`)
+        nodoResultado.innerText = `El resultado de ${primerNumero} / ${segundoNumero} es igual a ${calculoDivision}`
+    } else {
+        nodoResultado.className = (`error`)
+        nodoResultado.innerText = `Completa los campos correctamente`
+    }
+}
